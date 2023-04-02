a03c2e63cf4fa05473d7f844d9bcfff35d9d1773
amansindhal <jaswalgurmit@gmail.com>
2023-04-02 12:55:28 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1
+let num2 = 62
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 7) + 1;
+
+let num3=14
+let num4= 10
+let mod = num3 % num4;
+
+let max = Math.max(1,3,8,20,5,17,18,19)
+console.log(max)
